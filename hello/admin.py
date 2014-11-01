8dc5a3a96c831240891ab91a7afc11b04651a1b7
jkr <jimmy.kjaersgaard@gmail.com>
2014-11-01 20:51:38 +0100

    registered models in admin site

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from hello.models import User
+from hello.models import Discipline
+from hello.models import Points
+from hello.models import Standings
+from hello.models import Tournament
+from hello.models import Post
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Discipline)
+admin.site.register(Points)
+admin.site.register(Standings)
+admin.site.register(Tournament)
+admin.site.register(Post)
