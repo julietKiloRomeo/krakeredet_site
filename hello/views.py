77a822db8cbe0211d3d1834775f8c5d99e09aed9
jkr <jimmy.kjaersgaard@gmail.com>
2014-11-01 22:03:41 +0100

    added an index view

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Greeting
-import os
+
+from models import User
 
 # Create your views here.
 def index(request):
-    times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
+    users = User.objects.all()
+    return render(request, 'index.html', {'users': users})
 
 def db(request):
 
@@ -17,4 +17,5 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
 
