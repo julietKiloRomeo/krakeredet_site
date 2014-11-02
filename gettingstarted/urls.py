bce21e9045a48564dd3c930af150214ac9252de5
jkr <jimmy.kjaersgaard@gmail.com>
2014-11-02 09:24:52 +0100

    tried to add image path to urls, so they can be served correctly

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -15,3 +18,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
 )
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
